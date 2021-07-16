7cc876a3d4c8beba2b4386b8f65631fd8d25a8c0
xfz11 <81600993+xfz11@users.noreply.github.com>
2021-07-16 13:18:02 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -55,11 +55,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('RESOURCECONNECTOR_PG_HOST', '127.0.0.1'),
+            'port' => env('RESOURCECONNECTOR_PG_PORT', '5432'),
+            'database' => env('RESOURCECONNECTOR_PG_NAME', 'forge'),
+            'username' => env('RESOURCECONNECTOR_PG_USERNAME', 'forge'),
+            'password' => env('RESOURCECONNECTOR_PG_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
