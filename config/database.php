501139a15de8029e6376d53ede70e7d5c58a10c6
xfz11 <81600993+xfz11@users.noreply.github.com>
2021-07-16 13:16:54 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -40,12 +40,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'host' => env('RESOURCECONNECTOR_DB_HOST', '127.0.0.1'),
+            'port' => env('RESOURCECONNECTOR_DB_PORT', '3306'),
+            'database' => env('RESOURCECONNECTOR_DB_NAME', 'forge'),
+            'username' => env('RESOURCECONNECTOR_DB_USERNAME', 'forge'),
+            'password' => env('RESOURCECONNECTOR_DB_PASSWORD', ''),
+            'unix_socket' => env('RESOURCECONNECTOR_DB_SOCK', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
